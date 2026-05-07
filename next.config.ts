6f7dc9fbfb121aa59ac6a13c146f0791a2ad437e
Bintangpat <patrawijayabintang@gmail.com>
2026-05-07 14:34:05 +0700

    chore: remove root directory redirect in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: true, // 301 redirect (SEO friendly)
-      },
-    ];
-  },
-
   images: {
     domains: ["placehold.co"],
   },
